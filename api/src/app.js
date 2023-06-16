f72f47c9258805595ac1ed927af57acb011e4ac9
sr4mu <simon.ramu@gmail.com>
2023-06-16 16:21:44 +0200

    wip bdd

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,48 @@
-// const { Sequelize, DataTypes } = require('sequelize');
-
+const { Sequelize, DataTypes } = require('sequelize');
 // //Créez une instance de Sequelize et spécifiez les informations de connexion
-// const sequelize = new Sequelize('testdb', 'testuser', 'testpw', {
-//   host: 'postgres',
-//   port: 5432,
-//   dialect: 'postgres'
-// });
+const sequelize = new Sequelize('testdb', 'testuser', 'testpw', {
+  host: 'postgres',
+  port: 5432,
+  dialect: 'postgres'
+});
 
+const Product = sequelize.define('Product', {
+  productId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+}, {
+  underscored: true,
+});
+
+async function connectdb() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connexion OK');
+  } catch (error) {
+    console.error('FAIL Connexion:', error);
+  }
+}
+
+connectdb();
+
+async function getAllProducts(req, res, next) {
+  try {
+    const products = await Product.findAll();
+    const productsJSON = products.map(product => product.toJSON());
+    console.log('Products (JSON):', productsJSON);
+  } catch (err) {
+    console.log('Error:', err);
+    next(err);
+  }
+}
+
+getAllProducts();
 
 console.log('Hello');
 
